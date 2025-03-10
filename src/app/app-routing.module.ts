cb1a9d72be6f23e51e279710cb547049338f0927
MioraPriscaa <priscamiora9@gmail.com>
2025-03-10 15:42:57 +0100

    configure route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './page/accueil/accueil.component';
+import { LoginComponent } from './page/login/login.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-
-
+  { path: 'login', component: LoginComponent },
+  { path: 'accueil', component: AccueilComponent, canActivate: [AuthGuard]},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Page par défaut
 ];
 
 @NgModule({
