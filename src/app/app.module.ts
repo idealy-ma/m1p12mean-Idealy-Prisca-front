427fc0d1e5d7190d6b592debe17ce04353b7a8d0
MioraPriscaa <priscamiora9@gmail.com>
2025-03-11 09:33:14 +0100

    update route and app module for new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module'; 
 // Importations de Angular Material (si nécessaire)
-import { MAT_DATE_FORMATS, DateAdapter } from '@angular/material/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AccueilComponent } from './page/accueil/accueil.component';
 import { LoginComponent } from './page/login/login.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorService } from './services/interceptor/auth-interceptor.service';
+import { InscriptionComponent } from './page/inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent, // Composant principal
-    LoginComponent
+    LoginComponent, InscriptionComponent
   ],
   imports: [
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule,
     BrowserModule, // Module de base pour l'application Angular
     HttpClientModule, // Module pour effectuer des requêtes HTTP
