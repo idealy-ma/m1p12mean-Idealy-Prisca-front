3429e8d04b1d5efcb2bcb7d634d1fe7fc63511b0
MioraPriscaa <priscamiora9@gmail.com>
2025-03-13 20:44:22 +0100

    create component add-vehicule

---
+++ src/app/page/add-vehicule/add-vehicule.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Vehicule } from '../../models/vehicule.model';
+import { VehiculeService } from '../../services/vehicules/vehicule.service';
+
+@Component({
+  selector: 'app-add-vehicule',
+  templateUrl: './add-vehicule.component.html',
+  styleUrls: ['./add-vehicule.component.css']
+})
+export class AddVehiculeComponent implements OnInit {
+  vehiculeForm!: FormGroup;
+  todayDate = new Date().toISOString().split('T')[0]; // Format YYYY-MM-DD
+  selectedFiles: File[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private vehiculeService: VehiculeService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.vehiculeForm = this.fb.group({
+      immatricule: ['', Validators.required],
+      marque: ['', Validators.required],
+      modele: ['', Validators.required],
+    });
+  }
+
+  onFileSelect(event: any) {
+    this.selectedFiles = Array.from(event.target.files);
+  }
+
+  onSubmit() {
+    if (this.vehiculeForm.valid) {
+      const vehiculeData: Vehicule = {
+        immatricule: this.vehiculeForm.value.immatricule,
+        marque: this.vehiculeForm.value.marque,
+        modele: this.vehiculeForm.value.modele,
+        dateAjout: new Date(), // Date actuelle au format Date
+        photos: this.selectedFiles.map(file => file.name)
+      };
+  
+      this.vehiculeService.createVehicule(vehiculeData).subscribe({
+        next: (response) => {
+          console.log('Véhicule ajouté avec succès', response);
+          this.router.navigate(['/problemes']);
+        },
+        error: (err) => {
+          console.error('Erreur lors de l\'ajout du véhicule', err);
+        },
+      });
+    }
+  }
+  
+}
