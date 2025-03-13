9e481a9cff03115c7a526169cb5ee186b0c375e3
MioraPriscaa <priscamiora9@gmail.com>
2025-03-13 20:49:49 +0100

    function for logout and redirection in accueil component

--- src/app/page/accueil/accueil.component.ts
+++ src/app/page/accueil/accueil.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/authentification/auth.service';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent implements OnInit {
+export class AccueilComponent{
 
-  constructor() { }
+  constructor(private authService: AuthService,private router: Router) {}
 
-  ngOnInit(): void {
+  logout(): void {
+    this.authService.logout();
   }
-
+  goToAddVehicule(): void {
+    this.router.navigate(['/client/addvehicules']);
+  }
 }
