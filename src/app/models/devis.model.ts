7904b453927fdca7af1c970c2ecc8fcfff4e8a30
idealy-ma <r.idealy.ma@gmail.com>
2025-03-16 08:19:50 +0300

    Create devis model

---
+++ src/app/models/devis.model.ts
@@ -0,0 +1,22 @@
+export interface Devis {
+  _id?: string;
+  client?: {
+    _id: string;
+    nom: string;
+    prenom: string;
+    email: string;
+    id?: string;
+  };
+  vehicule?: {
+    _id: string;
+    marque: string;
+    modele: string;
+    immatricule: string;
+  };
+  description?: string;
+  dateCreation?: Date;
+  status?: string;
+  montantEstime?: number;
+  commentaire?: string;
+  id?: string;
+} 
