73dbf5a3a34e830ef03f9845bb322a0e2e034540
MioraPriscaa <priscamiora9@gmail.com>
2025-04-01 13:19:36 +0100

    configure environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://m1p12mean-idealy-prisca-back.onrender.com/api/v1'
+  apiUrl: 'https://m1p12mean-idealy-prisca-back.onrender.com/api/v1',
+  supabaseUrl: 'https://hsjvxudncotyfhccnqas.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzanZ4dWRuY290eWZoY2NucWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MDkwNTgsImV4cCI6MjA1OTA4NTA1OH0.Q91fV3yY1W9VX5cRYeDIgvOzIs_nhufnave7izHDedM'
+
 }; 
